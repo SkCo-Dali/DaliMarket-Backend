0999714caa198c3f8ece5181a457d9009c18f34f
agarciam19 <agarciam@skandia.com.co>
2025-09-16 12:18:57 -0500

    fix: Ensure requirements are installed before starting the application

--- startup.sh
+++ startup.sh
@@ -5,5 +5,7 @@ set -e
 apt-get update
 apt-get install -y msodbcsql17 unixodbc-dev
 
+pip install -r requirements.txt
+
 # Arrancar la aplicación con uvicorn
 python -m uvicorn presentation.main:app --host 0.0.0.0 --port 8000
